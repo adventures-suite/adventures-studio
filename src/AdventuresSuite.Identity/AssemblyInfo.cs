762b6d38c38a45b7953fd75d655066c1fe877ab7
Steve Simonton <ssimonton007@hotmail.com>
2026-08-10 06:49:03 -0700

    Extract shared authorization contracts

--- src/AdventuresSuite.Identity/AssemblyInfo.cs
+++ src/AdventuresSuite.Identity/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("TheSimontonAdventures.Web")]
 [assembly: InternalsVisibleTo("TheSimontonAdventures.Web.Tests")]
+[assembly: InternalsVisibleTo("AdventuresSuite.Authorization")]
